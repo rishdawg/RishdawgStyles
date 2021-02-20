b4262677d0e008d5a0d0384d0197050ac3c22e2a
rishdawg <rishirocks421@gmail.com>
2021-02-20 00:20:10 -0500

    lol I knew this would not work

--- stories/demo.stories.jsx
+++ stories/demo.stories.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../rishdawg-styles.scss';
 
 export const Demo = () => (
-  <div>
+  <div className="rish-container">
    <header className="rish-col rish-col-center">
      <h1> Rishdawg Cooks</h1>
      <img src="https://cookbookpic.s3.us-east-2.amazonaws.com/RishiBhai.png" alt="boy" class="w3-image"/>
